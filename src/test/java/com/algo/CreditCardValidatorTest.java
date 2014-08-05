aea4ffb7975bd9b2eb6184c9251ae5a07307f198
ashutoshtripathi <ashu.goldi@gmail.com>
2014-08-05 15:12:35 +0530

    updated test cases

--- src/test/java/com/algo/CreditCardValidatorTest.java
+++ src/test/java/com/algo/CreditCardValidatorTest.java
@@ -16,12 +16,15 @@ public class CreditCardValidatorTest {
 
     @Test
     public void shouldPassValidNumber() {
+    	
         Assert.assertEquals("0 is not a accetable card number", "invalidFormat", validator.validate("0"));
         Assert.assertEquals("1234 is not a accetable card number","invalidFormat", validator.validate("1234"));
         Assert.assertEquals("abcd is not a accetable card number","alphaError", validator.validate("abcd"));
         Assert.assertEquals("4525555544445555 must be valid card number","valid", validator.validate("4525555544445555"));
         Assert.assertEquals("4525555544445577 is invalid card number","invalid", validator.validate("4525555544445577"));
         Assert.assertEquals("Card must be of 16 digits only", "invalidFormat", validator.validate("452555554444557712212121"));
+        Assert.assertEquals("1234-5678-9012-3452 must be valid card number","valid", validator.validate("1234-5678-9012-3452"));
+        Assert.assertEquals("1234 5678 9012 3452 must be valid card number","valid", validator.validate("1234 5678 9012 3452"));
     }
 
 }
